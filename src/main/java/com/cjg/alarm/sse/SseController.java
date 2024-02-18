ae895ed3c3715e5f173d33ddf8afacaac30992f2
coolcjg <coolcjg@naver.com>
2024-02-18 20:41:12 +0900

    cors설정 추가, SSE : 클라이언트와 연결 확인

---
+++ src/main/java/com/cjg/alarm/sse/SseController.java
@@ -0,0 +1,45 @@
+package com.cjg.alarm.sse;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/sse")
+@RequiredArgsConstructor
+public class SseController {
+	
+	Logger logger = LoggerFactory.getLogger(SseController.class);
+	private final SseService sseService;
+	
+	@GetMapping(value = "", consumes = MediaType.ALL_VALUE)
+	public SseEmitter streamSseMvc(@RequestParam(value="userId") String userId) {
+		
+		//현재 클라이언트를 위한 SssEmitter 생성
+		SseEmitter emitter = new SseEmitter(Long.MAX_VALUE);
+		
+		try {
+			emitter.send(SseEmitter.event().name("connect"));
+		}catch(IOException e) {
+			logger.error(e.toString());
+		}
+		
+		sseService.saveEmitter(userId, emitter);
+			
+		emitter.onCompletion(() -> sseService.removeEmitter(userId));
+		emitter.onTimeout(()->sseService.removeEmitter(userId));
+		emitter.onError((e) -> sseService.removeEmitter(userId));
+		
+		return emitter;
+	}
+	
+}
---
+++ src/main/java/com/cjg/alarm/sse/SseService.java
@@ -0,0 +1,38 @@
+package com.cjg.alarm.sse;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+@Service
+public class SseService {
+	
+	public static Map<String, SseEmitter> sseEmitters = new ConcurrentHashMap<>();
+	
+	Logger logger = LoggerFactory.getLogger(SseService.class);
+	
+	public void saveEmitter(String userId, SseEmitter emitter) {
+		sseEmitters.put(userId, emitter);
+	}
+	
+	public void removeEmitter(String userId) {
+		sseEmitters.remove(userId);
+	}
+	
+	public void send(String userId, String message) {
+		SseEmitter sseEmitter = sseEmitters.get(userId);
+		
+		try {
+			sseEmitter.event().name("alarm").data(message);
+		}catch(Exception e) {
+			logger.error(e.toString());
+			sseEmitters.remove(userId);
+		}
+		
+	}
+
+}
